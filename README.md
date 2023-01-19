a88a186848c7f811cba286e2441e53666b546579
ivanbianko <ivan@krafteq.de>
2023-01-19 17:02:31 +0100

    initial project structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ops-community-streams
-Handles deployment of community streams
+
+Infrastructure-as-a-code repository to handle deployment of community streams using pulumi.
+Deploys kubernetes resources to Proxima clusters.
---
+++ index.ts
@@ -0,0 +1,12 @@
+import * as proxima from "@proxima-one/pulumi-service-apps";
+
+const communityStreams = new proxima.StreamingAppDeployer({
+  targetStack: "buh",
+  targetDb: {type: "import-kafka", name: "core-us"},
+  tuningArgs: {batch: 500, readBuffer: 10000},
+  stateManager: {type: "import", name: "state-manager-secondary"},
+  availableDbs: [], // provide readonly access to other streamdbs
+  maxUndoMs: 600 * 1000,
+});
+
+export const streamDbs = [communityStreams.getTargetDb()];
